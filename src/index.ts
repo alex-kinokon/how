3042cff320eebae3fcf04228257a9c7569b0b159
Alex <8125011+alex-kinokon@users.noreply.github.com>
2023-03-24 15:22:46 +0000

    Remove lib

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import os from "os";
 import { execSync } from "child_process";
 import chalk from "chalk";
 import { highlight } from "cli-highlight";
-import { sendMessage } from "./chatgpt";
 
 const platform = (() => {
   switch (os.platform()) {
@@ -19,13 +18,65 @@ const platform = (() => {
 
 const args = process.argv.slice(2).join(" ");
 
+async function sendMessage(prompt: string) {
+  const res = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "content-type": "application/json",
+      authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      messages: [
+        {
+          role: "system",
+          content: `You are a coding assistant and trained large language model. Respond with code that is as concisely as possible and do not add comments. Today is ${new Date().toLocaleString()}}.`,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 0.8,
+      max_tokens: 512,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      model: "gpt-3.5-turbo",
+      stream: false,
+    }),
+  });
+
+  const json = (await res.json()) as {
+    id: `chatcmpl-${string}`;
+    object: "chat.completion";
+    created: number; // seconds since epoch
+    model: "gpt-3.5-turbo-0301";
+    usage: {
+      prompt_tokens: number;
+      completion_tokens: number;
+      total_tokens: number;
+    };
+    choices: {
+      message: {
+        role: "assistant";
+        content: string;
+      };
+      finish_reason: "stop";
+      index: number;
+    }[];
+  };
+
+  return json.choices[0].message.content;
+}
+
 async function main() {
   const res = await sendMessage(
     `How ${args}? I'm on ${platform} cli. Reply in a Markdown code block and don't add any comment or explanation. ` +
       `Your entire answer must be machine executable and start with \`\`\`. Do not add "related" commands.`
   );
 
-  const answer = res.text
+  const answer = res
     .trim()
     .replace(/^```(sh|bash)?/gm, "")
     .replace(/```$/gm, "")
